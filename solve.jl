6e29585540cd5ee5f28c5c1d5d5899a481adf8c3
Francesco Minnocci <ascoli.minnocci@gmail.com>
2023-08-23 14:58:33 +0200

    chore: add multithreaded plots, document tty plot

--- solve.jl
+++ solve.jl
@@ -45,15 +45,13 @@ end
 F = [x*y - 1, x^2 + y^2 - 4]
 T = [x*y - 1, x^2 + y^2 - 2]
 C = [x^3 - y + 5x^2 - 10, 2x^2 - y - 10]
-P = [x*y - 1, x*y]
 
 sF = filter(u -> imag(u[1]) < 0.1 && imag(u[2]) < 0.1, solve(F))
 sT = filter(u -> imag(u[1]) < 0.1 && imag(u[2]) < 0.1, solve(T))
 sC = filter(u -> imag(u[1]) < 0.1 && imag(u[2]) < 0.1, solve(C))
-#  sP = filter(u -> imag(u[1]) < 0.1 && imag(u[2]) < 0.1, solve(P))
 
 # Plotting the system and the real solutions
+# ENV["GKSwstype"]="nul"                  
 plot_real(sF, F, 4, 4, "1")
 plot_real(sT, T, 4, 4, "2")
 plot_real(sC, C, 6, 12, "3")
-#  plot_real(sP, P, 5, 5, "4")
